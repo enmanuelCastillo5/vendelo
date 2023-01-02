41ead7a0356b9e3bf9995909630dfb6a7aded2f9
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2023-01-01 21:44:35 -0400

    login and protected pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend  
   
+  before_action :set_current_user
+  before_action :protect_pages
   before_action :set_locale
     
     def extract_locale
@@ -12,5 +14,13 @@ class ApplicationController < ActionController::Base
     private
     def set_locale
       I18n.locale = extract_locale || I18n.default_locale
+    end
+
+    def set_current_user
+      Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  
+    def protect_pages
+      redirect_to new_session_path, alert: t('common.not_logged_in') unless Current.user
     end
 end
